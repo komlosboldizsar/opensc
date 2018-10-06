d77d33364eb5257b53c50cab7b4bed9623473857
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2018-10-06 18:57:50 +0200

    Set base properties of MainForm

--- OpenSC/MainForm.Designer.cs
+++ OpenSC/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace OpenSC
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(982, 553);
+            this.IsMdiContainer = true;
+            this.Name = "MainForm";
+            this.Text = "OpenSC";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
