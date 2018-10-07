f0a399ca5b9883f38ccb080ea8119cf278539d74
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2018-10-07 15:17:22 +0200

    Added timer model

---
+++ OpenSC/TimerMode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenSC.Model.Timers
+{
+    public enum TimerMode
+    {
+        Forwards,
+        Backwards,
+        Clock
+    }
+}
