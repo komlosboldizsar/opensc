e4c42ad187646e26ad3cb7afd45f00d873930c58
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2018-10-13 12:21:54 +0200

    Add basic module manager

---
+++ OpenSC/Modules/IModule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenSC.Modules
+{
+    interface IModule
+    {
+        void ProgramStarted();
+        void MainWindowOpened();
+    }
+}
--- OpenSC/Program.cs
+++ OpenSC/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenSC.Modules;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -6,17 +7,30 @@ using System.Windows.Forms;
 
 namespace OpenSC
 {
+
+    public delegate void ProgramStartedDelegate();
+
     static class Program
     {
+
+        public static event ProgramStartedDelegate ProgramStarted;
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            ModuleManager.Init();
+            ProgramStarted?.Invoke();
+
             Application.Run(GUI.MainForm.Instance);
+
         }
+
     }
 }
