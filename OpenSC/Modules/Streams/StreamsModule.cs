62bcab3c24f5f92a999f2f760c283460b282400b
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2018-10-19 16:54:25 +0200

    Add "streams" module

---
+++ OpenSC/Modules/Streams/StreamsModule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenSC.Modules.Streams
+{
+    class StreamsModule
+    {
+
+        public void ProgramStarted()
+        {
+        }
+
+        public void MainWindowOpened()
+        {
+        }
+
+        public void RegisterDatabases()
+        {
+        }
+
+        public void RegisterWindowTypes()
+        {
+        }
+
+    }
+}
