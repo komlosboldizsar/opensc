98ada8f82025bed3135ee153fe8425cb7a991391
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2018-10-20 11:07:33 +0200

    Add enum to string/bitmap converters

---
+++ OpenSC/GUI/Helpers/Converters/EnumToStringConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenSC.GUI.Helpers.Converters
+{
+    class EnumToStringConverter<TEnum> : EnumConverter<TEnum, string>
+    {
+        public EnumToStringConverter(Dictionary<TEnum, string> convertedValues) : base(convertedValues)
+        {
+        }
+    }
+}
