99dde0e1703d45a6f29ede65ae52925a210c7bc9
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2018-10-20 19:53:35 +0200

    Add global settings manager

---
+++ OpenSC/Model/Settings/Converters/BoolConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenSC.Model.Settings.Converters
+{
+
+    [SettingValueConverter(typeof(bool))]
+    class BoolConverter : ISettingValueConverter
+    {
+
+        private const string TRUE = "true";
+        private const string FALSE = "false";
+
+        public string Serialize(object obj)
+        {
+            return (bool)obj ? TRUE : FALSE;
+        }
+
+        public object Deserialize(string serialized)
+        {
+            return (serialized == TRUE);
+        }
+
+    }
+
+}
