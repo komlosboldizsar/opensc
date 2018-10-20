e01e2c4fff84b9b2dcf897765d3c1295ff4c6f97
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2018-10-20 10:52:07 +0200

    Add EnumConverter to easily convert enums on forms to other value types

---
+++ OpenSC/GUI/Helpers/Converters/EnumConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenSC.GUI.Helpers.Converters
+{
+
+    class EnumConverter<TEnum, TConvertedValue>
+    {
+
+        private Dictionary<TEnum, TConvertedValue> convertedValues = new Dictionary<TEnum, TConvertedValue>();
+
+        public EnumConverter(Dictionary<TEnum, TConvertedValue> convertedValues)
+        {
+            if (convertedValues == null)
+                throw new ArgumentNullException();
+            foreach (var item in convertedValues)
+                this.convertedValues.Add(item.Key, item.Value);
+        }
+
+        public TConvertedValue Convert(TEnum key)
+        {
+            if (!convertedValues.TryGetValue(key, out TConvertedValue convertedValue))
+                return default(TConvertedValue);
+            return convertedValue;
+        }
+
+    }
+
+}
