7be3685bddc1e75566cf37b25831d672e5b5402c
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2018-10-21 14:13:16 +0200

    Add custom attributes for models: type code and type label

---
+++ OpenSC/Model/TypeCodeAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenSC.Model
+{
+
+    [AttributeUsage(AttributeTargets.Class)]
+    class TypeCodeAttribute : Attribute
+    {
+
+        public string Code { get; private set; }
+
+        public TypeCodeAttribute(string Code)
+        {
+            this.Code = Code;
+        }
+
+    }
+
+}
