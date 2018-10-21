c3dd72a218927f8558eefb79869a2fc75fa0c99d
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2018-10-21 13:37:55 +0200

    Add common elements of editor forms

---
+++ OpenSC/GUI/GeneralComponents/EditorForms/EditorGroup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpenSC.GUI.GeneralComponents.EditorForms
+{
+    public partial class EditorGroup : UserControl
+    {
+
+        [Category("Text"), Description("Caption of the contained group box.")]
+        public string GroupBoxText
+        {
+            get { return groupBox.Text; }
+            set { groupBox.Text = value; }
+        }
+
+        public EditorGroup()
+        {
+            InitializeComponent();
+        }
+    }
+}
