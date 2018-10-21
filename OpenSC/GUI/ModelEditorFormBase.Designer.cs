f488a9c7425d66343a50faf74a0076d04f72b53d
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2018-10-21 11:40:08 +0200

    Add base editor form

---
+++ OpenSC/GUI/ModelEditorFormBase.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace OpenSC.GUI
+{
+    partial class ModelEditorFormBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.saveAndCloseButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.mainContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainContainer
+            // 
+            this.mainContainer.Controls.Add(this.deleteButton);
+            this.mainContainer.Controls.Add(this.saveButton);
+            this.mainContainer.Controls.Add(this.cancelButton);
+            this.mainContainer.Controls.Add(this.saveAndCloseButton);
+            this.mainContainer.Padding = new System.Windows.Forms.Padding(10);
+            this.mainContainer.Size = new System.Drawing.Size(482, 337);
+            // 
+            // saveAndCloseButton
+            // 
+            this.saveAndCloseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.saveAndCloseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.saveAndCloseButton.Location = new System.Drawing.Point(332, 289);
+            this.saveAndCloseButton.Name = "saveAndCloseButton";
+            this.saveAndCloseButton.Size = new System.Drawing.Size(137, 35);
+            this.saveAndCloseButton.TabIndex = 3;
+            this.saveAndCloseButton.Text = "Save and close";
+            this.saveAndCloseButton.UseVisualStyleBackColor = true;
+            this.saveAndCloseButton.Click += new System.EventHandler(this.saveAndCloseButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cancelButton.Location = new System.Drawing.Point(154, 288);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(83, 35);
+            this.cancelButton.TabIndex = 4;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.saveButton.Location = new System.Drawing.Point(243, 289);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(83, 35);
+            this.saveButton.TabIndex = 5;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.deleteButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.deleteButton.Location = new System.Drawing.Point(13, 288);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(83, 35);
+            this.deleteButton.TabIndex = 6;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // ModelEditorFormBase
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 393);
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(500, 440);
+            this.Name = "ModelEditorFormBase";
+            this.Text = "Edit something";
+            this.mainContainer.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button saveAndCloseButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button deleteButton;
+    }
+}
---
+++ OpenSC/GUI/ModelEditorFormBase.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace OpenSC.GUI
+{
+    public partial class ModelEditorFormBase : ChildWindowWithTitle
+    {
+
+        [Category("Buttons"), Description("Visibility of Delete button at bottom-left.")]
+        public bool DeleteButtonVisible
+        {
+            get { return deleteButton.Visible; }
+            set { deleteButton.Visible = value; }
+        }
+
+        public ModelEditorFormBase()
+        {
+            InitializeComponent();
+            Load += windowLoaded;
+        }
+
+        private void windowLoaded(object sender, EventArgs e)
+        {
+            loadData();
+        }
+
+        protected virtual void loadData()
+        { }
+
+        protected virtual bool saveData()
+        {
+            return true;
+        }
+
+        protected virtual void delete()
+        { }
+
+        private void saveAndCloseButton_Click(object sender, EventArgs e)
+        {
+            if (saveData())
+                Close();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            saveData();
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            delete();
+        }
+
+    }
+}
