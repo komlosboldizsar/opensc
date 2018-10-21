17a0f1e7cf20834f7bca68299e4bff9ba66577d8
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2018-10-21 11:11:40 +0200

    Fix: put editor form type registers to the right namespace

--- OpenSC/GUI/Streams/StreamEditorFormTypeRegister.cs
+++ OpenSC/GUI/Streams/StreamEditorFormTypeRegister.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OpenSC.GUI.UMDs
+namespace OpenSC.GUI.VTRs
 {
     class StreamEditorFormTypeRegister: ModelEditorFormTypeRegister<Stream>
     {
