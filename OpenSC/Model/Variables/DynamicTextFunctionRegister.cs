9a223c8233a7b77af3ae3b9095952d15a26e6061
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2018-10-26 13:25:45 +0200

    Add register for dynamic texts

---
+++ OpenSC/Model/Variables/DynamicTextFunctionRegister.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenSC.Model.Variables
+{
+
+    public class DynamicTextFunctionRegister
+    {
+
+        public static DynamicTextFunctionRegister Instance { get; } = new DynamicTextFunctionRegister();
+
+        private DynamicTextFunctionRegister()
+        { }
+
+        private Dictionary<string, IDynamicTextFunction> registeredFunctions = new Dictionary<string, IDynamicTextFunction>();
+
+        public IReadOnlyList<IDynamicTextFunction> RegisteredFunctions
+        {
+            get => registeredFunctions.Values.ToList();
+        }
+
+        public void RegisterFunction(IDynamicTextFunction function)
+        {
+            string functionName = function.FunctionName;
+            if (registeredFunctions.ContainsKey(functionName))
+                throw new Exception();
+            registeredFunctions[functionName] = function;
+        }
+
+        public IDynamicTextFunction GetFunction(string name)
+        {
+            if (!registeredFunctions.TryGetValue(name, out IDynamicTextFunction function))
+                return null;
+            return function;
+        }
+
+        public IDynamicTextFunctionSubstitute GetFunctionSubstitute(string name, object[] arguments)
+        {
+            if (!registeredFunctions.TryGetValue(name, out IDynamicTextFunction function))
+                return null;
+            return function.GetSubstitute(arguments);
+        }
+
+    }
+
+}
