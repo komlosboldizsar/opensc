1be15c9187c5a45d1453da4ca6be8f097ddf915e
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2018-10-28 20:16:51 +0100

    Add observable list interface and implementation

---
+++ OpenSC/Model/General/IObservableList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenSC.Model.General
+{
+
+    public delegate void ObservableListItemAddedDelegate();
+    public delegate void ObservableListItemRemovedDelegate();
+    public delegate void ObservableListItemsChangedDelegate();
+
+    public interface IObservableList
+    {
+        event ObservableListItemAddedDelegate ItemAdded;
+        event ObservableListItemRemovedDelegate ItemRemoved;
+        event ObservableListItemsChangedDelegate ItemsChanged;
+    }
+
+}
