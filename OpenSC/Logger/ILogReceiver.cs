9538ae6d124bbdc1e89dffdcb4439681c7842e00
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2018-10-28 22:24:58 +0100

    Add logging base classes

---
+++ OpenSC/Logger/ILogReceiver.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenSC.Logger
+{
+    public interface ILogReceiver
+    {
+        void ReceiveLogMessage(LogMessageType messageType, string tag, string message);
+    }
+}
