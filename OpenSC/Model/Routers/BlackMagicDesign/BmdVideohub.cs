daa96ab2df6fba9819d73065f56d8df77cf02f81
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2018-11-02 13:48:27 +0100

    Fix: persist IP of BMD videohub model

--- OpenSC/Model/Routers/BlackMagicDesign/BmdVideohub.cs
+++ OpenSC/Model/Routers/BlackMagicDesign/BmdVideohub.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenSC.Model.Persistence;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -44,6 +45,7 @@ namespace OpenSC.Model.Routers.BlackMagicDesign
         public event ParameterlessChangeNotifierDelegate IpAddressChangingPCN;
         public event ParameterlessChangeNotifierDelegate IpAddressChangedPCN;
 
+        [PersistAs("ip_address")]
         private string ipAddress;
 
         public string IpAddress
