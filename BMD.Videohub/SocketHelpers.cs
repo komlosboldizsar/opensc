48fb98ca8f614c9d2032b2d22b7b9e6ba3053c8e
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2018-11-02 12:43:30 +0100

    Add BMD videohub implementation in separate project

---
+++ BMD.Videohub/SocketHelpers.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Sockets;
+
+namespace BMD.Videohub
+{
+
+    public static class SocketHelpers
+    {
+
+        // https://stackoverflow.com/a/722265
+        public static bool IsConnected(this Socket socket)
+        {
+            try
+            {
+                return !(socket.Poll(1, SelectMode.SelectRead) && socket.Available == 0);
+            }
+            catch (SocketException)
+            {
+                return false;
+            }
+        }
+
+    }
+
+}
