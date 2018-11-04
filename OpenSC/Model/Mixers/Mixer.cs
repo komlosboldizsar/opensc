3374d123b863ae5cf08f9fb17831b5e120249fa4
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2018-11-04 15:07:11 +0100

    Add OnProgramInput(Name) and OnPreviewInput(Name) properties to Mixer class

--- OpenSC/Model/Mixers/Mixer.cs
+++ OpenSC/Model/Mixers/Mixer.cs
@@ -15,6 +15,14 @@ namespace OpenSC.Model.Mixers
     public delegate void MixerNameChangingDelegate(Mixer mixer, string oldName, string newName);
     public delegate void MixerNameChangedDelegate(Mixer mixer, string oldName, string newName);
 
+    public delegate void MixerOnProgramInputChangingDelegate(Mixer mixer, MixerInput oldInput, MixerInput newInput);
+    public delegate void MixerOnProgramInputChangedDelegate(Mixer mixer, MixerInput oldInput, MixerInput newInput);
+    public delegate void MixerOnProgramInputNameChangedDelegate(Mixer mixer, string newName);
+    
+    public delegate void MixerOnPreviewInputChangingDelegate(Mixer mixer, MixerInput oldInput, MixerInput newInput);
+    public delegate void MixerOnPreviewInputChangedDelegate(Mixer mixer, MixerInput oldInput, MixerInput newInput);
+    public delegate void MixerOnPreviewInputNameChangedDelegate(Mixer mixer, string newName);
+
     public delegate void MixerInputsChangedDelegate(Mixer mixer);
 
     public abstract class Mixer : IModel
@@ -95,6 +103,112 @@ namespace OpenSC.Model.Mixers
         }
         #endregion
 
+        #region Property: OnProgramInput, OnProgramInputName
+        public event MixerOnProgramInputChangingDelegate OnProgramInputChanging;
+        public event MixerOnProgramInputChangedDelegate OnProgramInputChanged;
+        public event ParameterlessChangeNotifierDelegate OnProgramInputChangingPCN;
+        public event ParameterlessChangeNotifierDelegate OnProgramInputChangedPCN;
+
+        public event MixerOnProgramInputNameChangedDelegate OnProgramInputNameChanged;
+        public event ParameterlessChangeNotifierDelegate OnProgramInputNameChangedPCN;
+
+        private MixerInput onProgramInput;
+
+        public MixerInput OnProgramInput
+        {
+            get { return onProgramInput; }
+            protected set
+            {
+
+                if (value == onProgramInput)
+                    return;
+                MixerInput oldInput = onProgramInput;
+
+                if (onProgramInput != null)
+                    onProgramInput.NameChanged -= onProgramInputNameChangedHandler;
+
+                OnProgramInputChanging?.Invoke(this, oldInput, value);
+                OnProgramInputChangingPCN?.Invoke();
+
+                onProgramInput = value;
+
+                OnProgramInputChanged?.Invoke(this, oldInput, value);
+                OnProgramInputChangedPCN?.Invoke();
+
+                OnProgramInputNameChanged?.Invoke(this, onProgramInput?.Name);
+                OnProgramInputNameChangedPCN?.Invoke();
+
+                if (onProgramInput != null)
+                    onProgramInput.NameChanged += onProgramInputNameChangedHandler;
+
+            }
+        }
+
+        public string OnProgramInputName
+        {
+            get => onProgramInput?.Name;
+        }
+
+        private void onProgramInputNameChangedHandler(MixerInput input, string oldName, string newName)
+        {
+            OnProgramInputNameChanged?.Invoke(this, newName);
+            OnProgramInputNameChangedPCN?.Invoke();
+        }
+        #endregion
+
+        #region Property: OnPreviewInput, OnPreviewInputName
+        public event MixerOnPreviewInputChangingDelegate OnPreviewInputChanging;
+        public event MixerOnPreviewInputChangedDelegate OnPreviewInputChanged;
+        public event ParameterlessChangeNotifierDelegate OnPreviewInputChangingPCN;
+        public event ParameterlessChangeNotifierDelegate OnPreviewInputChangedPCN;
+
+        public event MixerOnPreviewInputNameChangedDelegate OnPreviewInputNameChanged;
+        public event ParameterlessChangeNotifierDelegate OnPreviewInputNameChangedPCN;
+
+        private MixerInput onPreviewInput;
+
+        public MixerInput OnPreviewInput
+        {
+            get { return onPreviewInput; }
+            protected set
+            {
+
+                if (value == onPreviewInput)
+                    return;
+                MixerInput oldInput = onPreviewInput;
+
+                if (onPreviewInput != null)
+                    onPreviewInput.NameChanged -= onPreviewInputNameChangedHandler;
+
+                OnPreviewInputChanging?.Invoke(this, oldInput, value);
+                OnPreviewInputChangingPCN?.Invoke();
+
+                onPreviewInput = value;
+
+                OnPreviewInputChanged?.Invoke(this, oldInput, value);
+                OnPreviewInputChangedPCN?.Invoke();
+
+                OnPreviewInputNameChanged?.Invoke(this, onPreviewInput?.Name);
+                OnPreviewInputNameChangedPCN?.Invoke();
+
+                if (onPreviewInput != null)
+                    onPreviewInput.NameChanged += onPreviewInputNameChangedHandler;
+
+            }
+        }
+
+        public string OnPreviewInputName
+        {
+            get => onPreviewInput?.Name;
+        }
+
+        private void onPreviewInputNameChangedHandler(MixerInput input, string oldName, string newName)
+        {
+            OnPreviewInputNameChanged?.Invoke(this, newName);
+            OnPreviewInputNameChangedPCN?.Invoke();
+        }
+        #endregion
+
         #region Inputs
         private ObservableList<MixerInput> inputs = new ObservableList<MixerInput>();
 
@@ -126,6 +240,10 @@ namespace OpenSC.Model.Mixers
         {
             inputs.Remove(input);
             input.RemovedFromMixer(this);
+            if (input == onProgramInput)
+                OnProgramInput = null;
+            if (input == onPreviewInput)
+                OnPreviewInput = null;
         }
 
         private void inputsChangedHandler()
