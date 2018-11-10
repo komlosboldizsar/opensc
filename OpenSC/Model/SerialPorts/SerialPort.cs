4bf92a7266f8cf90daccbd37c2cd661ec2b9ba74
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2018-11-10 17:43:26 +0100

    Add serial port model with base properties (ID, name) and database

---
+++ OpenSC/Model/SerialPorts/SerialPort.cs
@@ -0,0 +1,72 @@
+﻿using OpenSC.Model.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenSC.Model.SerialPorts
+{
+
+    public class SerialPort : ModelBase
+    {
+
+        #region Property: ID
+        public delegate void IdChangedDelegate(SerialPort port, int oldValue, int newValue);
+        public event IdChangedDelegate IdChanged;
+
+        private int id = 0;
+
+        public override int ID
+        {
+            get { return id; }
+            set
+            {
+                ValidateId(value);
+                int oldValue = id;
+                id = value;
+                IdChanged?.Invoke(this, oldValue, value);
+                RaisePropertyChanged(nameof(ID));
+            }
+        }
+
+        public void ValidateId(int id)
+        {
+            if (id <= 0)
+                throw new ArgumentException();
+            if (!SerialPortDatabase.Instance.CanIdBeUsedForItem(id, this))
+                throw new ArgumentException();
+        }
+        #endregion
+
+        #region Property: Name
+        public delegate void NameChangedDelegate(SerialPort port, string oldName, string newName);
+        public event NameChangedDelegate NameChanged;
+
+        [PersistAs("name")]
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value == name)
+                    return;
+                string oldName = name;
+                name = value;
+                NameChanged?.Invoke(this, oldName, value);
+                RaisePropertyChanged(nameof(Name));
+            }
+        }
+
+        public void ValidateName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException();
+        }
+        #endregion
+
+    }
+
+}
