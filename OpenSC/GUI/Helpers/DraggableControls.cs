4e2b1fdc31a3a56d8ffbc392e7ca5cedead5365c
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2019-11-23 16:28:49 +0100

    Change cursor when moving controls by drag'n'drop

--- OpenSC/GUI/Helpers/DraggableControls.cs
+++ OpenSC/GUI/Helpers/DraggableControls.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,7 +15,9 @@ namespace OpenSC.GUI.Helpers
 
         private static Dictionary<Control, bool> draggables = new Dictionary<Control, bool>();
 
-        private static System.Drawing.Size mouseOffset;
+        private static Size mouseOffset;
+
+        private static Cursor originalCursor;
         public static void Draggable(this Control control, bool Enable)
         {
             if (Enable)
@@ -25,7 +28,6 @@ namespace OpenSC.GUI.Helpers
                 control.MouseDown += new MouseEventHandler(control_MouseDown);
                 control.MouseUp += new MouseEventHandler(control_MouseUp);
                 control.MouseMove += new MouseEventHandler(control_MouseMove);
-
             }
             else
             {
@@ -35,25 +37,30 @@ namespace OpenSC.GUI.Helpers
                 control.MouseUp -= control_MouseUp;
                 control.MouseMove -= control_MouseMove;
                 draggables.Remove(control);
-
             }
         }
         static void control_MouseDown(object sender, MouseEventArgs e)
         {
-            mouseOffset = new System.Drawing.Size(e.Location);
-            draggables[(Control)sender] = true;
+            mouseOffset = new Size(e.Location);
+            Control typedSender = (Control)sender;
+            draggables[typedSender] = true;
+            originalCursor = typedSender.Cursor;
+            typedSender.Cursor = Cursors.SizeAll;
         }
         static void control_MouseUp(object sender, MouseEventArgs e)
         {
-            draggables[(Control)sender] = false;
+            Control typedSender = (Control)sender;
+            draggables[typedSender] = false;
+            typedSender.Cursor = originalCursor;
         }
         static void control_MouseMove(object sender, MouseEventArgs e)
         {
-            if (draggables[(Control)sender] == true)
+            Control typedSender = (Control)sender;
+            if (draggables[typedSender] == true)
             {
-                System.Drawing.Point newLocationOffset = e.Location - mouseOffset;
-                ((Control)sender).Left += newLocationOffset.X;
-                ((Control)sender).Top += newLocationOffset.Y;
+                Point newLocationOffset = e.Location - mouseOffset;
+                typedSender.Left += newLocationOffset.X;
+                typedSender.Top += newLocationOffset.Y;
             }
         }
 
