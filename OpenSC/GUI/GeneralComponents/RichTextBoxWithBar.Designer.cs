9f8dcc9f811d3dec6b18785b0533180d45be07aa
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2019-11-24 19:35:14 +0100

    Add RichTextBox with sidebar for showing points and tooltips Needs refactor

---
+++ OpenSC/GUI/GeneralComponents/RichTextBoxWithBar.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace OpenSC.GUI.GeneralComponents
+{
+    partial class RichTextBoxWithBar
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(24, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(317, 268);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.VScroll += new System.EventHandler(this.richTextBox1_VScroll);
+            this.richTextBox1.FontChanged += new System.EventHandler(this.richTextBox1_FontChanged);
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(24, 268);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            // 
+            // RichTextBoxWithBar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "RichTextBoxWithBar";
+            this.Size = new System.Drawing.Size(341, 268);
+            this.Load += new System.EventHandler(this.RichTextBoxWithBar_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
---
+++ OpenSC/GUI/GeneralComponents/RichTextBoxWithBar.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpenSC.GUI.GeneralComponents
+{
+    public partial class RichTextBoxWithBar : UserControl
+    {
+
+        public RichTextBox TextBox
+        {
+            get => this.richTextBox1;
+        }
+
+        public int BarWidth
+        {
+            get => panel1.Width;
+            set
+            {
+                panel1.Width = value;
+                drawPoints();
+            }
+        }
+
+        private int circleSize;
+        public int CircleSize
+        {
+            get => circleSize;
+            set
+            {
+                circleSize = value;
+                drawPoints();
+            }
+        }
+
+        public RichTextBoxWithBar()
+        {
+            InitializeComponent();
+        }
+
+        private Dictionary<int, PointData> pointData = new Dictionary<int, PointData>();
+
+        public void SetPointColor(int line, Color color, string tooltip = "")
+        {
+            if (line < 0)
+                throw new ArgumentException("Line number must be a non-negative integer!", nameof(line));
+            pointData[line] = new PointData(color, tooltip);
+            drawPoints();
+        }
+
+        public void RemovePoint(int line)
+        {
+            if (!pointData.ContainsKey(line))
+                return;
+            pointData.Remove(line);
+            drawPoints();
+        }
+        private void richTextBox1_VScroll(object sender, EventArgs e)
+        {
+            drawPoints();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            drawPoints();
+        }
+
+        private void richTextBox1_FontChanged(object sender, EventArgs e)
+        {
+            drawPoints();
+        }
+
+        private void RichTextBoxWithBar_Load(object sender, EventArgs e)
+        {
+            drawPoints();
+        }
+
+        private void drawPoints()
+        {
+            panel1.Invalidate();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+            Point basePosition = TextBox.GetPositionFromCharIndex(0);
+            float lineHeight = 0;
+            if (TextBox.Lines.Length > 1)
+            {
+                int secondLineFirstCharIndex = TextBox.GetFirstCharIndexFromLine(1);
+                Point secondPosition = TextBox.GetPositionFromCharIndex(secondLineFirstCharIndex);
+                lineHeight = secondPosition.Y - basePosition.Y;
+            }
+
+            float drawSize = (CircleSize >= lineHeight) ? (lineHeight - 1) : CircleSize;
+            float yBase = basePosition.Y + lineHeight / 2 + 2;
+            float xCenter = BarWidth / 2.0f;
+            float x = xCenter - (drawSize / 2.0f);
+            for (int i = 0; i < richTextBox1.Lines.Length; i++)
+            {
+                float yCenter = yBase + i * lineHeight;
+                float y = yCenter - (drawSize / 2.0f);
+                if (pointData.TryGetValue(i, out PointData pd))
+                    e.Graphics.FillEllipse(new SolidBrush(pd.Color), x, y, drawSize, drawSize);
+            }
+
+
+        }
+
+        private class PointData
+        {
+            public Color Color { get; private set; }
+            public string Tooltip { get; private set; }
+            public PointData(Color color, string tooltip)
+            {
+                this.Color = color;
+                this.Tooltip = tooltip;
+            }
+        }
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            int activeTooltip = -1;
+
+            Point basePosition = TextBox.GetPositionFromCharIndex(0);
+            float lineHeight = 0;
+            if (TextBox.Lines.Length > 1)
+            {
+                int secondLineFirstCharIndex = TextBox.GetFirstCharIndexFromLine(1);
+                Point secondPosition = TextBox.GetPositionFromCharIndex(secondLineFirstCharIndex);
+                lineHeight = secondPosition.Y - basePosition.Y;
+            }
+
+            float drawSize = (CircleSize >= lineHeight) ? (lineHeight - 1) : CircleSize;
+            float yBase = basePosition.Y + lineHeight / 2 + 2;
+            float xCenter = BarWidth / 2.0f;
+            float x = xCenter - (drawSize / 2.0f);
+            for (int i = 0; i < richTextBox1.Lines.Length; i++)
+            {
+                float yCenter = yBase + i * lineHeight;
+                float y = yCenter - (drawSize / 2.0f);
+                if (Math.Sqrt(Math.Pow((e.X - xCenter), 2) + Math.Pow((e.Y - yCenter), 2)) <= drawSize)
+                    activeTooltip = i;
+            }
+
+            if ((activeTooltip >= 0) && pointData.TryGetValue(activeTooltip, out PointData pd) && !string.IsNullOrEmpty(pd.Tooltip))
+                toolTip1.Show(pd.Tooltip, panel1, e.Location + new Size(10, 10));
+            else
+                toolTip1.Hide(ParentForm);
+
+        }
+
+    }
+}
