8cf2243f827a7e08f9b4942beafa151a0a51c545
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2019-11-25 17:45:40 +0100

    Add TimerReachedValue macro trigger

---
+++ OpenSC/Model/Timers/Triggers/TimerMacroTriggers.cs
@@ -0,0 +1,24 @@
+﻿using OpenSC.Model.Macros;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenSC.Model.Timers.Triggers
+{
+
+    public class TimerMacroTriggers : MacroTriggerRegister.IMacroTriggerCollection
+    {
+
+        public static readonly IMacroTrigger TimerReachedValue = new TimerReachedValueMacroTrigger();
+        
+        public static readonly MacroTriggerRegister.IMacroTriggerCollection Instance = new TimerMacroTriggers();
+
+        public IMacroTrigger[] TriggersToRegister => new IMacroTrigger[] {
+            TimerReachedValue
+        };
+
+    }
+
+}
