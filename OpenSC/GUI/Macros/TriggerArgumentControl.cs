b07fd51d9ea04c6952aac61cbd361a6fa14d775e
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2019-11-27 19:07:48 +0100

    String and numerical type arguments can be edited on MacroEditorForm

--- OpenSC/GUI/Macros/TriggerArgumentControl.cs
+++ OpenSC/GUI/Macros/TriggerArgumentControl.cs
@@ -34,40 +34,104 @@ namespace OpenSC.GUI.Macros
 
         private bool last;
 
-        private static readonly Type[] TYPES_TEXTBOX = new Type[]
-        {
-            typeof(string)
-        };
-
-        private static readonly Type[] TYPES_NUMERICFIELD = new Type[]
-        {
-            typeof(int)
-        };
+        private Control valueField;
 
         private void TriggerArgumentControl_Load(object sender, EventArgs e)
         {
+
             if (last)
                 borderPanel.Visible = false;
+
             if (argument == null)
                 return;
+
             argumentIndexLabel.Text = string.Format("(arg{0})", argIndex);
             argumentNameLabel.Text = argument.Name;
             argumentDescriptionLabel.Text = argument.Description;
-            valueComboBox.CreateAdapterAsDataSource<object>(argument.GetPossibilities(new object[] { }), item => argument.GetStringForPossibility(item), true, "-");
+
+            if (argument.Type == typeof(string))
+            {
+                table.Controls.Remove(valueComboBox);
+                TextBox valueTextBox = new TextBox();
+                table.Controls.Add(valueTextBox, 1, 2);
+                valueTextBox.Dock = DockStyle.Top;
+                valueField = valueTextBox;
+            }
+            else if (isNumericalType(argument.Type))
+            {
+
+                table.Controls.Remove(valueComboBox);
+
+                NumericUpDown valueNumericField = new NumericUpDown();
+                valueNumericField.Minimum = 0;
+                valueNumericField.Maximum = 99999;
+                bool isInt = (argument.Type == typeof(int));
+                valueNumericField.DecimalPlaces = isInt ? 0 : 2;
+                valueNumericField.Increment = isInt ? 1 : (decimal)0.05;
+
+                table.Controls.Add(valueNumericField, 1, 2);
+                valueNumericField.Dock = DockStyle.Top;
+                valueField = valueNumericField;
+
+            }
+            else
+            {
+                valueComboBox.CreateAdapterAsDataSource<object>(argument.GetPossibilities(new object[] { }), item => argument.GetStringForPossibility(item), true, "-");
+                valueField = valueComboBox;
+            }
+
         }
+
+        private static bool isNumericalType(Type type)
+            => ((type == typeof(int)) || (type == typeof(decimal)) || (type == typeof(float)) || (type == typeof(double)));
 
         public object[] PreviousArgumentValues
         {
             set
             {
+                if ((argument.Type == typeof(string)) || isNumericalType(argument.Type))
+                    return;
                 valueComboBox.CreateAdapterAsDataSource<object>(argument.GetPossibilities(value), item => argument.GetStringForPossibility(item), true, "-");
             }
         }
 
         public object ArgumentValue
         {
-            get => valueComboBox.SelectedValue;
-            set { valueComboBox.SelectByValue(value); }
+            get
+            {
+                TextBox valueTextBox = valueField as TextBox;
+                if (valueTextBox != null)
+                    return valueTextBox.Text;
+                NumericUpDown valueNumericField = valueField as NumericUpDown;
+                if (valueNumericField != null)
+                {
+                    if (argument.Type == typeof(int))
+                        return (int)valueNumericField.Value;
+                    if (argument.Type == typeof(float))
+                        return (float)valueNumericField.Value;
+                    if (argument.Type == typeof(double))
+                        return (double)valueNumericField.Value;
+                    return valueNumericField.Value;
+                }
+                return valueComboBox.SelectedValue;
+            }
+            set
+            {
+                TextBox valueTextBox = valueField as TextBox;
+                if (valueTextBox != null)
+                {
+                    valueTextBox.Text = value?.ToString() ?? "";
+                    return;
+                }
+                NumericUpDown valueNumericField = valueField as NumericUpDown;
+                if (valueNumericField != null) {
+                    if (!decimal.TryParse((value?.ToString() ?? ""), out decimal valueDecimal))
+                        valueDecimal = 0;
+                    valueNumericField.Value = valueDecimal;
+                    return;
+                }
+                valueComboBox.SelectByValue(value);
+            }
         }
 
         public delegate void ArgumentValueChangedDelegate(TriggerArgumentControl control, IMacroTriggerArgument argument, object newValue);
