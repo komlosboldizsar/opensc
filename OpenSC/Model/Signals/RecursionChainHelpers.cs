8087e6a151731328603246d9068166202a0e0088
Boldizsár Komlós <komlos.boldizsar@gmail.com>
2021-05-10 17:46:53 +0200

    Add RecursionChainHelpers to easily create and extend recursion chains

---
+++ OpenSC/Model/Signals/RecursionChainHelpers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenSC.Model.Signals
+{
+
+    public static class RecursionChainHelpers
+    {
+
+        public static List<T> ExtendRecursionChainT<T>(this List<T> recursionChain, T newElement) {
+            if (recursionChain == null)
+                recursionChain = new List<T>();
+            recursionChain.Add(newElement);
+            return recursionChain;
+        }
+
+        public static List<object> ExtendRecursionChain(this List<object> recursionChain, object newElement)
+            => ExtendRecursionChainT(recursionChain, newElement);
+
+        public static List<T> CreateRecursionChainT<T>(T firstElement)
+        {
+            List<T> recursionChain = new List<T>();
+            recursionChain.Add(firstElement);
+            return recursionChain;
+        }
+
+        public static List<object> CreateRecursionChain(object firstElement)
+            => CreateRecursionChainT(firstElement);
+
+    }
+
+}
