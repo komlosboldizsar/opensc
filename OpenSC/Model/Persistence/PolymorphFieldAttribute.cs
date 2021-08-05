4992990a21ded2c64fc7da41bbf0db6dc0d81625
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2021-08-05 11:35:04 +0200

    Serialize and deserialize polymorph fields

---
+++ OpenSC/Model/Persistence/PolymorphFieldAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenSC.Model.Persistence
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    class PolymorphFieldAttribute: Attribute
+    {
+
+        public delegate Dictionary<Type, string> TypeStringDictionaryGetterDelegate();
+
+        public TypeStringDictionaryGetterDelegate TypeStringDictionaryGetter { get; private set; }
+
+        public string TypeAttributeName { get; private set; }
+
+        public PolymorphFieldAttribute(TypeStringDictionaryGetterDelegate StringTypeDictionaryGetter, string TypeAttributeName = "type")
+        {
+            this.TypeStringDictionaryGetter = StringTypeDictionaryGetter;
+            this.TypeAttributeName = TypeAttributeName;
+        }
+
+    }
+}
