20b661f5d602c10b161534ea3f91369615f7a3c8
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2021-08-16 19:01:05 +0200

    Use GetAttribute() in other attribute helpers

--- OpenSC/Model/AttributeHelpers.cs
+++ OpenSC/Model/AttributeHelpers.cs
@@ -14,34 +14,13 @@ namespace OpenSC.Model
             => type.GetCustomAttributes(true).OfType<TAttribute>().FirstOrDefault();
 
         public static string GetTypeLabel(this Type type)
-        {
-            foreach(object attribute in type.GetCustomAttributes(true))
-            {
-                TypeLabelAttribute typedAttribute = attribute as TypeLabelAttribute;
-                if (typedAttribute != null)
-                    return typedAttribute.Label;
-            }
-            return string.Empty;
-        }
+            => type.GetAttribute<TypeLabelAttribute>()?.Label;
 
         public static string GetTypeCode(this Type type)
-        {
-            foreach (object attribute in type.GetCustomAttributes(true))
-            {
-                TypeCodeAttribute typedAttribute = attribute as TypeCodeAttribute;
-                if (typedAttribute != null)
-                    return typedAttribute.Code;
-            }
-            return string.Empty;
-        }
+            => type.GetAttribute<TypeCodeAttribute>()?.Code;
 
         public static string GetName(this IDatabaseBase database)
-        {
-            object[] databaseNameAttributes = database.GetType().GetCustomAttributes(typeof(DatabaseNameAttribute), true);
-            if (databaseNameAttributes.Length <= 0)
-                return null;
-            return (databaseNameAttributes[0] as DatabaseNameAttribute)?.Name;
-        }
+            => database.GetType().GetAttribute<DatabaseNameAttribute>()?.Name;
 
     }
 
