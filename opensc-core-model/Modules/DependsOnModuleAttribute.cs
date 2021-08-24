201cbd3c121b16a7a4d183ef8f7ad723511b9bbe
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2021-08-24 08:12:28 +0200

    Explicit allow inheritance of DependsOnModule attribute

--- opensc-core-model/Modules/DependsOnModuleAttribute.cs
+++ opensc-core-model/Modules/DependsOnModuleAttribute.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace OpenSC.Modules
 {
-    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = true)]
     public class DependsOnModuleAttribute : Attribute
     {
         public Type Module { get; private set; }
