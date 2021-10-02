9cdae19dc5428954134cc79bfb05cb71f1d1fc05
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2021-10-02 13:40:53 +0200

    Add GUI for bool setting

---
+++ opensc-core-gui/GUI/Settings/BoolSettingEditor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenSC.Model.Settings;
+using System.Diagnostics;
+
+namespace OpenSC.GUI.Settings
+{
+
+    [SettingEditorControlType(typeof(bool))]
+    public partial class BoolSettingEditor : SettingEditorBase
+    {
+
+        public BoolSettingEditor() : base() => InitializeComponent();
+        public BoolSettingEditor(ISetting setting) : base(setting) => InitializeComponent();
+
+        private void IntSettingEditor_Load(object sender, EventArgs e) => checkBox.Checked = ((Setting<bool>)setting).Value;
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            if (setting == null)
+                return;
+            try
+            {
+                ((Setting<bool>)setting).Value = checkBox.Checked;
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message, "Validation error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            if (setting != null)
+                checkBox.Checked = ((Setting<bool>)setting).Value;
+        }
+
+        public override ISettingEditorControl GetInstanceForSetting(ISetting setting) => new BoolSettingEditor(setting);
+
+    }
+
+}
