e179868249433ba332bf6657f62ae7c37430c9b3
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2021-10-03 14:47:24 +0200

    Add converter for EnumSetting

---
+++ opensc-core-model/Model/Settings/Converters/EnumSettingConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenSC.Model.Settings.Converters
+{
+
+    [SettingConverter(typeof(EnumSetting))]
+    class EnumSettingConverter : ISettingConverter
+    {
+
+        public string Serialize(ISetting setting) => setting.ObjValue?.ToString();
+
+        public void Deserialize(ISetting setting, string serialized)
+        {
+            EnumSetting enumSetting = setting as EnumSetting;
+            if (enumSetting == null)
+                return;
+            if (Enum.TryParse(enumSetting.EnumType, serialized, out object deserializedEnum))
+                setting.ObjValue = deserializedEnum;
+        }
+
+    }
+
+}
