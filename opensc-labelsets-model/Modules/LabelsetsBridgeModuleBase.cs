7a8856aedb344e86e353d1b0fe212fb1376bff23
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2021-10-12 10:36:14 +0200

    Add base module for bridges to labelsets

---
+++ opensc-labelsets-model/Modules/LabelsetsBridgeModuleBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenSC.Modules
+{
+
+    [DependsOnModule(typeof(LabelsetsModelModule))]
+    public abstract class LabelsetsBridgeModuleBase<TModuleFrom> : BridgeModuleBase<TModuleFrom, LabelsetsModelModule>
+    {
+
+        public override void Initialize()
+        {
+            startRegisteringLabelableObjects();
+        }
+
+        protected abstract void startRegisteringLabelableObjects();
+
+    }
+
+}
