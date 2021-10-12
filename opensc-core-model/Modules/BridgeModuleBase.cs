c4d12debb314e72e1d3fc79a149373e37439b0e5
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2021-10-12 11:32:42 +0200

    Add bridge module bases with 3 and 4 generic arguments (for modules with 1 "bridge from", 2/3 "bridge to" modules)

--- opensc-core-model/Modules/BridgeModuleBase.cs
+++ opensc-core-model/Modules/BridgeModuleBase.cs
@@ -2,8 +2,20 @@
 
 namespace OpenSC.Modules
 {
+
     public abstract class BridgeModuleBase<TModuleFrom, TModuleTo> : IModule
     {
         public abstract void Initialize();
     }
+
+    public abstract class BridgeModuleBase<TModuleFrom, TModuleTo1, TModuleTo2> : IModule
+    {
+        public abstract void Initialize();
+    }
+
+    public abstract class BridgeModuleBase<TModuleFrom, TModuleTo1, TModuleTo2, TModuleTo3> : IModule
+    {
+        public abstract void Initialize();
+    }
+
 }
