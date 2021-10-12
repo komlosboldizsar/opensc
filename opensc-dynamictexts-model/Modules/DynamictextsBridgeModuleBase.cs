d15ecc4c3a5a1900eb56edf9a24252f12e1b01b2
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2021-10-12 11:46:47 +0200

    Add DynamictextsBridgeModuleBase with 2/3 generic "TModuleFrom" parameters

--- opensc-dynamictexts-model/Modules/DynamictextsBridgeModuleBase.cs
+++ opensc-dynamictexts-model/Modules/DynamictextsBridgeModuleBase.cs
@@ -7,18 +7,25 @@ using System.Threading.Tasks;
 namespace OpenSC.Modules
 {
 
-
     [DependsOnModule(typeof(DynamictextsModelModule))]
     public abstract class DynamictextsBridgeModuleBase<TModuleFrom> : BridgeModuleBase<TModuleFrom, DynamictextsModelModule>
     {
-
-        public override void Initialize()
-        {
-            registerDynamicTextFunctions();
-        }
-
+        public override void Initialize() => registerDynamicTextFunctions();
         protected abstract void registerDynamicTextFunctions();
+    }
 
+    [DependsOnModule(typeof(DynamictextsModelModule))]
+    public abstract class DynamictextsBridgeModuleBase<TModuleFrom1, TModuleFrom2> : BridgeModuleBase<TModuleFrom1, TModuleFrom2, DynamictextsModelModule>
+    {
+        public override void Initialize() => registerDynamicTextFunctions();
+        protected abstract void registerDynamicTextFunctions();
+    }
+
+    [DependsOnModule(typeof(DynamictextsModelModule))]
+    public abstract class DynamictextsBridgeModuleBase<TModuleFrom1, TModuleFrom2, TModuleFrom3> : BridgeModuleBase<TModuleFrom1, TModuleFrom2, TModuleFrom3, DynamictextsModelModule>
+    {
+        public override void Initialize() => registerDynamicTextFunctions();
+        protected abstract void registerDynamicTextFunctions();
     }
 
 }
