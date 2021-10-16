e3b3887ef924097f1ae10d0e392de2d7ef6b472c
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2021-10-16 11:23:06 +0200

    Add again 2 router output related dynamic text function

---
+++ opensc-routers-labelsets-bridge-dynamictexts/Modules/RoutersLabelsetsBridgeDynamictextsModule.cs
@@ -0,0 +1,20 @@
+﻿using OpenSC.Model;
+using OpenSC.Model.Labelsets.DynamicTextFunctions;
+using OpenSC.Model.Variables;
+
+namespace OpenSC.Modules
+{
+
+    [Module("routers-labelsets-bridge-dynamictexts", "Routers with labelsets (bridge to dynamic texts)", "TODO")]
+    [DependsOnModule(typeof(LabelsetsModelModule))]
+    public class RoutersLabelsetsBridgeDynamictextsModule : DynamictextsBridgeModuleBase<RoutersModelModule, LabelsetsModelModule>
+    {
+
+        protected override void registerDynamicTextFunctions()
+        {
+            DynamicTextFunctionRegister.Instance.RegisterFunction(new ObjectLabel());
+        }
+
+    }
+
+}
