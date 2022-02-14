1d179e9001318582d67ab8adc0eb43eebec2af45
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2022-02-14 18:08:59 +0100

    Add new module with datetime dynamic text function

---
+++ opensc-dynamictexts-datetime/Modules/DynamictextsDatetimeModule.cs
@@ -0,0 +1,19 @@
+﻿using OpenSC.Model;
+using OpenSC.Model.Variables.DynamicTextFunctions;
+using OpenSC.Model.Variables;
+
+namespace OpenSC.Modules
+{
+
+    [Module("dynamictexts-functions-datetime", "Dynamictexts, datetime functions", "TODO")]
+    public class DynamictextsDatetimeModule : DynamictextsFunctionsModuleBase
+    {
+
+        protected override void registerDynamicTextFunctions()
+        {
+            DynamicTextFunctionRegister.Instance.RegisterFunction(new DatetimeDotnetFormat());
+        }
+
+    }
+
+}
