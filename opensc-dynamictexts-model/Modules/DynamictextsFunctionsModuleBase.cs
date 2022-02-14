9bf2cd0fa8b8f639502656f2ea46ad08d4978a7c
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2022-02-14 18:08:06 +0100

    Add module type: extension with "generic" dynamic text functions (e.g. time)

---
+++ opensc-dynamictexts-model/Modules/DynamictextsFunctionsModuleBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenSC.Modules
+{
+
+    [DependsOnModule(typeof(DynamictextsModelModule))]
+    public abstract class DynamictextsFunctionsModuleBase : IModule
+    {
+        public void Initialize() => registerDynamicTextFunctions();
+        protected abstract void registerDynamicTextFunctions();
+    }
+
+}
