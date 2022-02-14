800cb0530da7e4dfd0b2851f584aab0b1ae83a42
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2022-02-14 19:31:23 +0100

    Fix: ItemListFormBaseManager table initialization

--- opensc-core-gui/GUI/ItemListFormBaseManager.cs
+++ opensc-core-gui/GUI/ItemListFormBaseManager.cs
@@ -36,7 +36,7 @@ namespace OpenSC.GUI
             List<CustomDataGridViewColumnDescriptorBuilder<TItem>> columnDescriptorBuilders = new List<CustomDataGridViewColumnDescriptorBuilder<TItem>>();
             ColumnDescriptorBuilderGetterDelegate builderGetterMethod = () =>
             {
-                CustomDataGridViewColumnDescriptorBuilder<TItem> builder = new CustomDataGridViewColumnDescriptorBuilder<TItem>();
+                CustomDataGridViewColumnDescriptorBuilder<TItem> builder = new CustomDataGridViewColumnDescriptorBuilder<TItem>(table);
                 columnDescriptorBuilders.Add(builder);
                 return builder;
             };
