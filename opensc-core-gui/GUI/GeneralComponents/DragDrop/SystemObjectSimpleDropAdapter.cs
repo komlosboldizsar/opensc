0608557475a30b0fa9a16d072112a650f5788453
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2022-03-06 15:18:00 +0100

    Refactor ComboBoxSystemObjectDropAdapter, create separate base class

---
+++ opensc-core-gui/GUI/GeneralComponents/DragDrop/SystemObjectSimpleDropAdapter.cs
@@ -0,0 +1,34 @@
+﻿using OpenSC.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpenSC.GUI.GeneralComponents.DragDrop
+{
+
+    public class SystemObjectSimpleDropAdapter<TReceiver> : SystemObjectCompositeDropAdapter<TReceiver, SystemObjectSimpleDropAdapter<TReceiver>.NullChild>
+        where TReceiver : Control
+    {
+
+        public SystemObjectSimpleDropAdapter(SimpleReceiverDragDesponderDelegate receiverDragDesponder, SimpleReceiverValueSetterDelegate receiverValueSetter)
+            : base(nullChildSelector,
+                  (rp, rc, ea, t) => receiverDragDesponder(rp, ea, t),
+                  (rp, rc, so, ea, t) => receiverValueSetter(rp, so, ea, t),
+                  nullChild)
+        { }
+
+        public delegate bool SimpleReceiverDragDesponderDelegate(TReceiver receiver, DragEventArgs eventArgs, object tag);
+        public delegate void SimpleReceiverValueSetterDelegate(TReceiver receiver, ISystemObject systemObject, DragEventArgs eventArgs, object tag);
+
+        private static NullChild nullChildSelector(TReceiver receiver, DragEventArgs eventArgs) => null;
+
+        private static NullChild nullChild = new NullChild();
+
+        public class NullChild { }
+
+    }
+
+}
--- opensc-core-gui/GUI/GeneralComponents/DropDowns/ComboBoxSystemObjectDropAdapter.cs
+++ opensc-core-gui/GUI/GeneralComponents/DropDowns/ComboBoxSystemObjectDropAdapter.cs
@@ -1,4 +1,5 @@
-﻿using OpenSC.Model;
+﻿using OpenSC.GUI.GeneralComponents.DragDrop;
+using OpenSC.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,87 +13,30 @@ namespace OpenSC.GUI.GeneralComponents.DropDowns
     public static class ComboBoxSystemObjectDropAdapter
     {
 
-        public static void ReceiveSystemObjectDrop(this ComboBox comboBox)
+        private static SystemObjectSimpleDropAdapter<ComboBox> baseInstance;
+        private static SystemObjectSimpleDropAdapter<ComboBox> BaseInstance
         {
-            comboBox.AllowDrop = true;
-            comboBox.Disposed += disposedHandler;
-            comboBox.DragEnter += dragEnterHandler;
-            comboBox.DragDrop += dragDropHandler;
-            if (!dropDataDictionary.TryGetValue(comboBox, out DropData dropData))
+            get
             {
-                dropData = new DropData() { ComboBox = comboBox };
-                dropDataDictionary.Add(comboBox, dropData);
+                if (baseInstance == null)
+                    baseInstance = new SystemObjectSimpleDropAdapter<ComboBox>(receiverDragResponder, receiverValueSetter);
+                return baseInstance;
             }
         }
 
-        public static void FilterSystemObjectDropByType<T>(this ComboBox comboBox)
+        private static void receiverValueSetter(ComboBox receiver, ISystemObject systemObject, DragEventArgs eventArgs, object tag)
         {
-            if (!dropDataDictionary.TryGetValue(comboBox, out DropData dropData))
-                return;
-            dropData.TypeFilters.Add(new TypeFilter<T>());
+            if (receiver.ContainsValue(systemObject))
+                receiver.SelectByValue(systemObject);
         }
 
-        private static void disposedHandler(object sender, EventArgs e)
-        {
-            if (sender is ComboBox comboBox)
-                dropDataDictionary.Remove(comboBox);
-        }
+        private static bool receiverDragResponder(ComboBox receiver, DragEventArgs eventArgs, object tag) => true;
 
-        private static void dragEnterHandler(object sender, DragEventArgs e)
-        {
-            if (!dragEventHandler(sender, e, out DropData dropData))
-                return;
-        }
+        public static void ReceiveSystemObjectDrop(this ComboBox comboBox)
+            => BaseInstance.ReceiveSystemObjectDrop(comboBox);
 
-        private static void dragDropHandler(object sender, DragEventArgs e)
-        {
-            if (!dragEventHandler(sender, e, out DropData dropData))
-                return;
-            if (!(e.Data.GetData(typeof(SystemObjectReference)) is SystemObjectReference systemObjectReference))
-                return;
-            ISystemObject systemObject = systemObjectReference.Object;
-            ComboBox comboBox = ((ComboBox)sender);
-            if (comboBox.ContainsValue(systemObject))
-                comboBox.SelectByValue(systemObject);
-        }
-
-        private static bool dragEventHandler(object sender, DragEventArgs e, out DropData dropData)
-        {
-            dropData = null;
-            e.Effect = DragDropEffects.None;
-            if (!(sender is ComboBox comboBox))
-                return false;
-            if (!e.Data.GetDataPresent(typeof(SystemObjectReference)))
-                return false;
-            if (!dropDataDictionary.TryGetValue(comboBox, out dropData))
-                return false;
-            if (dropData.TypeFilters.Count > 0) {
-                SystemObjectReference systemObjectReference = e.Data.GetData(typeof(SystemObjectReference)) as SystemObjectReference;
-                ISystemObject systemObject = systemObjectReference?.Object;
-                if (!dropData.TypeFilters.Any(tf => tf.Is(systemObject)))
-                    return false;
-            }
-            e.Effect = DragDropEffects.Link;
-            return true;
-        }
-
-        private static Dictionary<ComboBox, DropData> dropDataDictionary = new();
-
-        private class DropData
-        {
-            public ComboBox ComboBox { get; set; }
-            public List<ITypeFilter> TypeFilters { get; } = new();
-        }
-
-        private interface ITypeFilter
-        {
-            bool Is(object obj);
-        }
-
-        private class TypeFilter<T> : ITypeFilter
-        {
-            public bool Is(object obj) => obj is T;
-        }
+        public static void FilterSystemObjectDropByType<TSystemObject>(this ComboBox comboBox)
+            => BaseInstance.FilterSystemObjectDropByType<TSystemObject>(comboBox);
 
     }
 
