e66a79e69cb6df8d61429adba89ebc2a0092f11a
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2022-03-15 18:40:10 +0100

    Add module for booleans-related macro commands and triggers

---
+++ opensc-booleans-bridge-macros/Modules/BooleansBridgeMacrosModule.cs
@@ -0,0 +1,26 @@
+﻿using OpenSC.Model;
+using OpenSC.Model.Macros;
+//using OpenSC.Model.Booleans.Macros;
+//using OpenSC.Model.Booleans.Triggers;
+
+namespace OpenSC.Modules
+{
+
+    [Module("booleans-bridge-macros", "Booleans (bridge to macros)", "TODO")]
+    [DependsOnModule(typeof(BooleansModelModule))]
+    public class BooleansBridgeMacrosModule : MacrosBridgeModuleBase<BooleansModelModule>
+    {
+
+        protected override void registerMacroCommands()
+        {
+            //MacroCommandRegister.Instance.RegisterCommand(new XY());
+        }
+
+        protected override void registerTriggerTypes()
+        {
+            //MacroTriggerRegister.Instance.RegisterTrigger(new XY());
+        }
+
+    }
+
+}
