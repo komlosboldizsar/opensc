7ae3f47849f347ef9a6e6eb4beac9b28a4d5e40b
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2022-03-15 17:32:57 +0100

    Add bridge module for MIDI related macro commands and triggers

---
+++ opensc-midi-bridge-macros/Modules/MidiBridgeMacrosModule.cs
@@ -0,0 +1,21 @@
+﻿using OpenSC.Model;
+using OpenSC.Model.Macros;
+//using OpenSC.Model.Midi.Triggers;
+
+namespace OpenSC.Modules
+{
+
+    [Module("midi-bridge-macros", "Midi (bridge to macros)", "TODO")]
+    [DependsOnModule(typeof(MidiModelModule))]
+    public class MidiBridgeMacrosModule : MacrosBridgeModuleBase<MidiModelModule>
+    {
+
+        protected override void registerMacroCommands()
+        { }
+
+        protected override void registerTriggerTypes()
+        { }
+
+    }
+
+}
