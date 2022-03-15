287e07227fa092c01f59442fd56945e9ad8b647d
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2022-03-15 16:02:19 +0100

    Add opensc-midi-model project, install Sanford.Multimedia.Midi with NuGet

---
+++ opensc-midi-model/Modules/MidiModelModule.cs
@@ -0,0 +1,21 @@
+﻿using OpenSC.Model;
+using OpenSC.Model.Persistence;
+
+namespace OpenSC.Modules
+{
+
+    [Module("midi-model", "Midi (model)", "TODO")]
+    public class MidiModelModule : BasetypeModuleBase
+    {
+
+        protected override void registerDatabases()
+        {
+        }
+
+        protected override void registerSerializers()
+        {
+        }
+
+    }
+
+}
