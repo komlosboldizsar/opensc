735d6e9d41f561032d5aef9b33c1700f68cbb14d
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2022-04-08 21:50:29 +0200

    Add extension class for Control cloning

---
+++ opensc-core-gui/GUI/Helpers/Cloning.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpenSC.GUI.Helpers
+{
+
+    public static class Cloning
+    {
+
+        public static Control Clone(this Control baseInstance, string[] excludeProperties = null)
+        {
+            Type baseType = baseInstance.GetType();
+            PropertyInfo[] controlProperties = baseType.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            Control newInstance = (Control)Activator.CreateInstance(baseType);
+            foreach (PropertyInfo propInfo in controlProperties)
+                if (propInfo.CanWrite)
+                    if ((propInfo.Name != "WindowTarget") && (excludeProperties?.Contains(propInfo.Name) != true))
+                        propInfo.SetValue(newInstance, propInfo.GetValue(baseInstance, null), null);
+            return newInstance;
+        }
+
+        // @source https://stackoverflow.com/a/10267292
+        public static T CloneT<T>(this T baseInstance, string[] excludeProperties = null)
+            where T : Control
+        {
+            PropertyInfo[] controlProperties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            T newInstance = Activator.CreateInstance<T>();
+            foreach (PropertyInfo propInfo in controlProperties)
+                if (propInfo.CanWrite)
+                    if ((propInfo.Name != "WindowTarget") && (excludeProperties?.Contains(propInfo.Name) != true))
+                        propInfo.SetValue(newInstance, propInfo.GetValue(baseInstance, null), null);
+            return newInstance;
+        }
+
+    }
+
+}
