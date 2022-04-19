f370f078099069962d849ef18856cce7ffb64b86
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2022-04-19 20:58:50 +0200

    Refactor table layout row/column cloning

--- opensc-core-gui/GUI/Helpers/TableLayoutHelpers.cs
+++ opensc-core-gui/GUI/Helpers/TableLayoutHelpers.cs
@@ -12,75 +12,105 @@ namespace OpenSC.GUI.Helpers
     public static class TableLayoutHelpers
     {
 
-        public const int ROW_INDEX_LAST = -1;
+        public abstract class Cloner<TItemStyle>
+            where TItemStyle : TableLayoutStyle
+        {
+
+            protected TableLayoutPanel tableLayout;
+            protected int sourceIndex;
+            protected TItemStyle sourceStyle;
+
+            public Cloner(TableLayoutPanel tableLayout, int sourceIndex)
+            {
+                this.tableLayout = tableLayout;
+                this.sourceIndex = sourceIndex;
+                int itemCount = getItemCount();
+                if (itemCount <= sourceIndex)
+                    throw new ArgumentException();
+                setItemCount(itemCount + 1);
+                sourceStyle = getSourceItemStyle();
+            }
+
+            public const int DESTINATION_INDEX_LAST = -1;
+
+            public void DoCloning(int destinationIndex = DESTINATION_INDEX_LAST, string[] excludeProperties = null)
+            {
+                if (destinationIndex < 0)
+                    destinationIndex = getItemCount() + destinationIndex;
+                cloneSourceItemStyleTo(destinationIndex);
+                int newSourceIndex = sourceIndex;
+                if (destinationIndex <= sourceIndex)
+                    newSourceIndex++;
+                int orthogonalIndex = 0;
+                int orthogonalItemCount = getOrthogonalItemCount();
+                while (orthogonalIndex < orthogonalItemCount)
+                {
+                    Control originalControl = getControl(newSourceIndex, orthogonalIndex);
+                    if (originalControl != null)
+                    {
+                        int orthogonalSpan = getOrthogonalSpan(originalControl);
+                        Control clonedControl = originalControl.CloneTypeOnly();
+                        addControl(clonedControl, destinationIndex, orthogonalIndex);
+                        setOrthogonalSpan(clonedControl, orthogonalSpan);
+                        clonedControl.ClonePropertiesFrom(originalControl, excludeProperties);
+                        orthogonalIndex += orthogonalSpan;
+                    }
+                    else
+                    {
+                        orthogonalIndex++;
+                    }
+                }
+            }
+
+            protected abstract int getItemCount();
+            protected abstract void setItemCount(int value);
+            protected abstract int getOrthogonalItemCount();
+            protected abstract TItemStyle getSourceItemStyle();
+            protected abstract void cloneSourceItemStyleTo(int destinationIndex);
+            protected abstract Control getControl(int itemIndex, int orthogonalIndex);
+            protected abstract void addControl(Control control, int itemIndex, int orthogonalIndex);
+            protected abstract int getOrthogonalSpan(Control control);
+            protected abstract void setOrthogonalSpan(Control control, int value);
+
+        }
+
+        public class RowCloner : Cloner<RowStyle>
+        {
+            public RowCloner(TableLayoutPanel tableLayout, int sourceIndex) : base(tableLayout, sourceIndex) { }
+            protected override int getItemCount() => tableLayout.RowCount;
+            protected override void setItemCount(int value) => tableLayout.RowCount = value;
+            protected override int getOrthogonalItemCount() => tableLayout.ColumnCount;
+            protected override RowStyle getSourceItemStyle() => tableLayout.RowStyles[sourceIndex];
+            protected override void cloneSourceItemStyleTo(int destinationIndex) => tableLayout.RowStyles.Insert(destinationIndex, new RowStyle(sourceStyle.SizeType, sourceStyle.Height));
+            protected override Control getControl(int itemIndex, int orthogonalIndex) => tableLayout.GetControlFromPosition(orthogonalIndex, itemIndex);
+            protected override void addControl(Control control, int itemIndex, int orthogonalIndex) => tableLayout.Controls.Add(control, orthogonalIndex, itemIndex);
+            protected override int getOrthogonalSpan(Control control) => tableLayout.GetColumnSpan(control);
+            protected override void setOrthogonalSpan(Control control, int value) => tableLayout.SetColumnSpan(control, value);
+        }
+
+        public class ColumnCloner : Cloner<ColumnStyle>
+        {
+            public ColumnCloner(TableLayoutPanel tableLayout, int sourceIndex) : base(tableLayout, sourceIndex) { }
+            protected override int getItemCount() => tableLayout.ColumnCount;
+            protected override void setItemCount(int value) => tableLayout.ColumnCount = value;
+            protected override int getOrthogonalItemCount() => tableLayout.RowCount;
+            protected override ColumnStyle getSourceItemStyle() => tableLayout.ColumnStyles[sourceIndex];
+            protected override void cloneSourceItemStyleTo(int destinationIndex) => tableLayout.ColumnStyles.Insert(destinationIndex, new ColumnStyle(sourceStyle.SizeType, sourceStyle.Width));
+            protected override Control getControl(int itemIndex, int orthogonalIndex) => tableLayout.GetControlFromPosition(itemIndex, orthogonalIndex);
+            protected override void addControl(Control control, int itemIndex, int orthogonalIndex) => tableLayout.Controls.Add(control, itemIndex, orthogonalIndex);
+            protected override int getOrthogonalSpan(Control control) => tableLayout.GetRowSpan(control);
+            protected override void setOrthogonalSpan(Control control, int value) => tableLayout.SetRowSpan(control, value);
+        }
+
+        public const int ROW_INDEX_LAST = RowCloner.DESTINATION_INDEX_LAST;
 
         public static void CloneRow(this TableLayoutPanel tableLayout, int sourceIndex, int destinationIndex = ROW_INDEX_LAST, string[] excludeProperties = null)
-        {
-            if (tableLayout.RowCount <= sourceIndex)
-                throw new ArgumentException();
-            tableLayout.RowCount++; 
-            RowStyle sourceStyle = tableLayout.RowStyles[sourceIndex];
-            RowStyle destinationStyle = new RowStyle(sourceStyle.SizeType, sourceStyle.Height);
-            if (destinationIndex < 0)
-                destinationIndex = tableLayout.RowCount + destinationIndex;
-            tableLayout.RowStyles.Insert(destinationIndex, destinationStyle);
-            int newSourceIndex = sourceIndex;
-            if (destinationIndex <= sourceIndex)
-                newSourceIndex++;
-            int columnIndex = 0; 
-            while (columnIndex < tableLayout.ColumnCount)
-            {
-                Control originalControl = tableLayout.GetControlFromPosition(columnIndex, newSourceIndex);
-                if (originalControl != null)
-                {
-                    int columnSpan = tableLayout.GetColumnSpan(originalControl);
-                    Control clonedControl = originalControl.CloneTypeOnly();
-                    tableLayout.Controls.Add(clonedControl, columnIndex, destinationIndex);
-                    tableLayout.SetColumnSpan(clonedControl, columnSpan);
-                    clonedControl.ClonePropertiesFrom(originalControl, excludeProperties);
-                    columnIndex += columnSpan;
-                }
-                else
-                {
-                    columnIndex++;
-                }
-            }
-        }
+            => (new RowCloner(tableLayout, sourceIndex)).DoCloning(destinationIndex, excludeProperties);
 
-        public const int COLUMN_INDEX_LAST = -1;
+        public const int COLUMN_INDEX_LAST = ColumnCloner.DESTINATION_INDEX_LAST;
 
         public static void CloneColumn(this TableLayoutPanel tableLayout, int sourceIndex, int destinationIndex = COLUMN_INDEX_LAST, string[] excludeProperties = null)
-        {
-            if (tableLayout.ColumnCount <= sourceIndex)
-                throw new ArgumentException();
-            tableLayout.ColumnCount++;
-            ColumnStyle sourceStyle = tableLayout.ColumnStyles[sourceIndex];
-            ColumnStyle destinationStyle = new ColumnStyle(sourceStyle.SizeType, sourceStyle.Width);
-            if (destinationIndex < 0)
-                destinationIndex = tableLayout.ColumnCount + destinationIndex;
-            tableLayout.ColumnStyles.Insert(destinationIndex, destinationStyle);
-            int newSourceIndex = sourceIndex;
-            if (destinationIndex <= sourceIndex)
-                newSourceIndex++;
-            int rowIndex = 0;
-            while (rowIndex < tableLayout.RowCount)
-            {
-                Control originalControl = tableLayout.GetControlFromPosition(newSourceIndex, rowIndex);
-                if (originalControl != null)
-                {
-                    int rowSpan = tableLayout.GetRowSpan(originalControl);
-                    Control clonedControl = originalControl.CloneTypeOnly();
-                    tableLayout.Controls.Add(clonedControl, destinationIndex, rowIndex);
-                    tableLayout.SetRowSpan(clonedControl, rowSpan);
-                    clonedControl.ClonePropertiesFrom(originalControl, excludeProperties);
-                    rowIndex += rowSpan;
-                }
-                else
-                {
-                    rowIndex++;
-                }
-            }
-        }
+            => (new ColumnCloner(tableLayout, sourceIndex)).DoCloning(destinationIndex, excludeProperties);
 
     }
 
