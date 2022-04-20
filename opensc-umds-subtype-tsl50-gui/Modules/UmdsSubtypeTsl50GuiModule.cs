b739c4d1e248031d6696c1e778cd90dc9b3e4a25
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2022-04-20 11:20:51 +0200

    Change menu structure for TSL 5.0

--- opensc-umds-subtype-tsl50-gui/Modules/UmdsSubtypeTsl50GuiModule.cs
+++ opensc-umds-subtype-tsl50-gui/Modules/UmdsSubtypeTsl50GuiModule.cs
@@ -26,16 +26,22 @@ namespace OpenSC.Modules
 
         public const string TOPMENUGROUP_ID = UmdsGuiModule.TOPMENUGROUP_ID;
         public const string TOPMENU_LABEL = UmdsGuiModule.TOPMENU_LABEL;
-        public const string MENUGROUP_ID = "tsl50";
+        public const string MENUGROUP_ID = "subtypes";
         public const int MENUGROUP_WEIGHT = UmdsGuiModule.MENUGROUP_WEIGHT + 10;
-        public const string SUBMENU_LABEL = "TSL 5.0 screen list";
+        public const string SUBMENU_LABEL = "TSL 5.0";
+        public const string SUBMENU_GROUP_ID = "base";
+        public const int SUBMENU_GROUP_WEIGHT = 10;
+        public const string SUBMENU_ITEM_SCREENLIST_LABEL = "Screen list";
 
         protected void registerMenus()
         {
             MenuItem topMenu = MenuManager.Instance.TopMenu[TOPMENUGROUP_ID][TOPMENU_LABEL];
             MenuItemGroup menuGroup = topMenu[MENUGROUP_ID];
             menuGroup.Weight = MENUGROUP_WEIGHT;
-            menuGroup[SUBMENU_LABEL].ClickHandler = (menu, tag) => new Tsl50ScreenList().ShowAsChild();
+            MenuItem subMenu = menuGroup[SUBMENU_LABEL];
+            MenuItemGroup subMenuGroup = subMenu[SUBMENU_GROUP_ID];
+            subMenuGroup.Weight = SUBMENU_GROUP_WEIGHT;
+            subMenuGroup[SUBMENU_ITEM_SCREENLIST_LABEL].ClickHandler = (menu, tag) => new Tsl50ScreenList().ShowAsChild();
         }
 
     }
