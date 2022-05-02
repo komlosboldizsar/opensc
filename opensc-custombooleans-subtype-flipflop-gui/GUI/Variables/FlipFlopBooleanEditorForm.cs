e5fa40a8c953ef3691a2ad4da9161d9e3b88a5d9
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2022-05-02 22:43:53 +0200

    Fix input dropdowns in FlipFlopBooleanEditorForm

--- opensc-custombooleans-subtype-flipflop-gui/GUI/Variables/FlipFlopBooleanEditorForm.cs
+++ opensc-custombooleans-subtype-flipflop-gui/GUI/Variables/FlipFlopBooleanEditorForm.cs
@@ -71,8 +71,8 @@ namespace OpenSC.GUI.Variables
         private void initDropDowns()
         {
             typeDropDown.SetAdapterAsDataSource(new EnumComboBoxAdapter<FlipFlopType>(FLIPFLOP_TYPE_TRANSLATIONS));
-            input1DropDown.CreateAdapterAsDataSource(BooleanRegister.Instance, null, true, "(not associated)");
-            input2DropDown.CreateAdapterAsDataSource(BooleanRegister.Instance, null, true, "(not associated)");
+            input1DropDown.CreateAdapterAsDataSource(BooleanRegister.Instance, b => b.Identifier, true, "(not associated)");
+            input2DropDown.CreateAdapterAsDataSource(BooleanRegister.Instance, b => b.Identifier, true, "(not associated)");
         }
 
         private static readonly Dictionary<FlipFlopType, string> FLIPFLOP_TYPE_TRANSLATIONS = new()
