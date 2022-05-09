25f9585a491dbeac67e99470b33f7884038f8f60
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2022-05-09 15:32:51 +0200

    Add GUI for GPIO interfaces

---
+++ opensc-gpiointerfaces-gui/GUI/GpioInterfaces/GpioInterfaceEditorFormTypeRegister.cs
@@ -0,0 +1,10 @@
+﻿using OpenSC.Model.GpioInterfaces;
+
+namespace OpenSC.GUI.GpioInterfaces
+{
+    public class GpioInterfaceEditorFormTypeRegister : ModelEditorFormTypeRegister<GpioInterface>
+    {
+        public static ModelEditorFormTypeRegister<GpioInterface> Instance { get; } = new GpioInterfaceEditorFormTypeRegister();
+        private GpioInterfaceEditorFormTypeRegister() { }
+    }
+}
