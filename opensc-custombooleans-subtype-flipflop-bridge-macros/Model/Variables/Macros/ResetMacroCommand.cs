72c80edcee20db203a303c85a5dd84fe85ab656b
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2022-05-10 19:14:58 +0200

    Add set-reset-toggle macro commands for flip-flop booleans

---
+++ opensc-custombooleans-subtype-flipflop-bridge-macros/Model/Variables/Macros/ResetMacroCommand.cs
@@ -0,0 +1,15 @@
+﻿using OpenSC.Model.Macros;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenSC.Model.Variables.Macros
+{
+    [MacroCommand("CustomBooleans.FlipFlop.Rest", "Reset a flip-flop.", "Change the value of a flip-flop boolean to 0/off.")]
+    public class ResetMacroCommand : FlipFlopMacroCommandBase
+    {
+        protected override void changeState(FlipFlopBoolean flipFlop) => flipFlop.Reset();
+    }
+}
---
+++ opensc-custombooleans-subtype-flipflop-bridge-macros/Modules/CustombooleansSubtypeFlipflopBridgeMacrosModule.cs
@@ -0,0 +1,25 @@
+﻿using OpenSC.Model;
+using OpenSC.Model.Macros;
+using OpenSC.Model.Variables.Macros;
+
+namespace OpenSC.Modules
+{
+
+    [Module("custombooleans-subtype-flipflop-bridge-macros", "Custom booleans / FlipFlop (bridge to macros)", "TODO")]
+    [DependsOnModule(typeof(CustombooleansSubtypeFlipflopModelModule))]
+    public class CustombooleansSubtypeFlipflopBridgeMacrosModule : MacrosBridgeModuleBase<CustombooleansSubtypeFlipflopModelModule>
+    {
+
+        protected override void registerMacroCommands()
+        {
+            MacroCommandRegister.Instance.RegisterCommand(new AutoTransitionMacroCommand());
+            MacroCommandRegister.Instance.RegisterCommand(new CutTransitionMacroCommand());
+            MacroCommandRegister.Instance.RegisterCommand(new SetPreviewSourceMacroCommand());
+        }
+
+        protected override void registerTriggerTypes()
+        { }
+
+    }
+
+}
