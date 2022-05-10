ec448f49921b5c1247bea6ad97233a3d6555509a
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2022-05-10 19:05:22 +0200

    Add GUI for BMD tallybox

---
+++ opensc-gpiointerfaces-subtype-bmdtallybox-gui/Modules/GpiointerfacesSubtypeBmdtallyboxGuiModule.cs
@@ -0,0 +1,22 @@
+﻿using OpenSC.GUI.GpioInterfaces;
+using OpenSC.Model.GpioInterfaces.BlackMagicDesign;
+
+namespace OpenSC.Modules
+{
+
+    [Module("gpiointerfaces-subtype-bmdtallybox-gui", "GPIO intefaces / BlackMagic Design Tally Box (GUI)", "TODO")]
+    [DependsOnModule(typeof(GpioInterfacesSubtypeBmdtallyboxModelModule))]
+    public class GpiointerfacesSubtypeBmdtallyboxGuiModule : SubtypeGuiModuleBase<GpioInterfacesSubtypeBmdtallyboxModelModule>
+    {
+
+        protected override void registerPersistableWindowTypes()
+        { }
+
+        protected override void registerSubtypeEditorWindowTypes()
+        {
+            GpioInterfaceEditorFormTypeRegister.Instance.RegisterFormType<BmdTallyBox, BmdTallyBoxEditorForm>();
+        }
+
+    }
+
+}
