1a8a7e8248babf2cd7288d2fc458d85315746503
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2022-05-11 21:32:31 +0200

    Add X32 faders - model

---
+++ opensc-x32faders-model/Model/X32Faders/X32Fader.cs
@@ -0,0 +1,177 @@
+﻿using OpenSC.Logger;
+using OpenSC.Model.General;
+using OpenSC.Model.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenSC.Model.X32Faders
+{
+
+    public class X32Fader : ModelBase
+    {
+
+        public const string LOG_TAG = "X32Fader";
+
+        #region Persistence, instantiation
+        public X32Fader()
+        { }
+
+        public override void Removed()
+        { }
+        #endregion
+
+        #region Owner database
+        public override sealed IDatabaseBase OwnerDatabase { get; } = X32FaderDatabase.Instance;
+        #endregion
+
+        #region Property: IpAddress
+        public event PropertyChangedTwoValuesDelegate<X32Fader, string> IpAddressChanged;
+
+        [PersistAs("ip_address")]
+        private string ipAddress = "192.168.10.41";
+
+        public string IpAddress
+        {
+            get => ipAddress;
+            set => this.setProperty(ref ipAddress, value, IpAddressChanged);
+        }
+        #endregion
+
+        #region Property: OscPath
+        public event PropertyChangedTwoValuesDelegate<X32Fader, string> OscPathChanged;
+
+        [PersistAs("osc_path")]
+        private string oscPath = "";
+
+        public string OscPath
+        {
+            get => oscPath;
+            set => this.setProperty(ref oscPath, value, OscPathChanged);
+        }
+        #endregion
+
+        #region Property: TargetLevel
+        public event PropertyChangedTwoValuesDelegate<X32Fader, decimal> TargetLevelChanged;
+
+        [PersistAs("target_level")]
+        private decimal targetLevel = 1.0m;
+
+        public decimal TargetLevel
+        {
+            get => targetLevel;
+            set => this.setProperty(ref targetLevel, value, TargetLevelChanged, validator: ValidateTargetLevel);
+        }
+
+        public void ValidateTargetLevel(decimal level)
+        {
+            if ((level < 0) || (level > 1))
+                throw new ArgumentException();
+        }
+        #endregion
+
+        #region Property: Time
+        public event PropertyChangedTwoValuesDelegate<X32Fader, int> TimeChanged;
+
+        [PersistAs("time")]
+        private int time = 1000;
+
+        public int Time
+        {
+            get => time;
+            set => this.setProperty(ref time, value, TimeChanged, validator: ValidateTime);
+        }
+
+        public void ValidateTime(int time)
+        {
+            if (time < 0)
+                throw new ArgumentException();
+        }
+        #endregion
+
+        #region Property: ReferenceLevelForTime
+        public event PropertyChangedTwoValuesDelegate<X32Fader, decimal> ReferenceLevelForTimeChanged;
+
+        [PersistAs("reference_level_for_time")]
+        private decimal referenceLevelForTime = 0.0m;
+
+        public decimal ReferenceLevelForTime
+        {
+            get => referenceLevelForTime;
+            set => this.setProperty(ref referenceLevelForTime, value, ReferenceLevelForTimeChanged, validator: ValidateReferenceLevelForTime);
+        }
+
+        public void ValidateReferenceLevelForTime(decimal referenceLevelForTime)
+        {
+            if ((referenceLevelForTime < 0) || (referenceLevelForTime > 1))
+                throw new ArgumentException();
+        }
+        #endregion
+
+        #region Property: UseReferenceLevelForTime
+        public event PropertyChangedTwoValuesDelegate<X32Fader, bool> UseReferenceLevelForTimeChanged;
+
+        [PersistAs("use_reference_level_for_time")]
+        private bool useReferenceLevelForTime = false;
+
+        public bool UseReferenceLevelForTime
+        {
+            get => useReferenceLevelForTime;
+            set => this.setProperty(ref useReferenceLevelForTime, value, UseReferenceLevelForTimeChanged);
+        }
+        #endregion
+
+        #region Property: TimeStep
+        public event PropertyChangedTwoValuesDelegate<X32Fader, int> TimeStepChanged;
+
+        [PersistAs("time_step")]
+        private int timeStep = 50;
+
+        public int TimeStep
+        {
+            get => timeStep;
+            set => this.setProperty(ref timeStep, value, TimeStepChanged, validator: ValidateTimeStep);
+        }
+
+        public void ValidateTimeStep(int timeStep)
+        {
+            if (timeStep < 1)
+                throw new ArgumentException();
+        }
+        #endregion
+
+        public void Do() => Task.Run(_do);
+
+        private async void _do()
+        {
+            float startLevel = await queryCurrentLevel();
+            float startLevelDifference = (float)targetLevel - startLevel;
+            float fadeTime = time;
+            if (useReferenceLevelForTime)
+            {
+                float referenceLevelDifference = (float)(targetLevel - referenceLevelForTime);
+                fadeTime *= Math.Abs(startLevelDifference) / Math.Abs(referenceLevelDifference);
+            }
+            float currentLevel = startLevel;
+            int steps = (int)(fadeTime / timeStep);
+            float levelStep = startLevelDifference / steps;
+            for (int s = 0; s < steps; s++)
+            {
+                currentLevel += levelStep;
+                setLevel(currentLevel);
+                await Task.Delay(timeStep);
+            }
+            if (currentLevel != (float)targetLevel)
+                setLevel((float)targetLevel);
+        }
+
+        private async Task<float> queryCurrentLevel() => await X32FaderCommons.QueryCurrentLevel(oscPath, ipAddress);
+
+        private void setLevel(float level) => X32FaderCommons.SetLevel(oscPath, level, ipAddress);
+
+
+    }
+
+}
---
+++ opensc-x32faders-model/Model/X32Faders/X32FaderCommons.cs
@@ -0,0 +1,111 @@
+﻿using Bespoke.Osc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OpenSC.Model.X32Faders
+{
+    internal class X32FaderCommons
+    {
+
+        private const int SOURCE_PORT = 10024;
+        private const int DESTINATION_PORT = 10023;
+
+        public static async Task<float> QueryCurrentLevel(string path, string ip, int port = DESTINATION_PORT)
+        {
+            TaskCompletionSource<float> request = addRequest(path, ip, port);   
+            sendQueryCurrentLevel(path, ip, port);
+            return await request.Task;
+        }
+
+        private static void sendQueryCurrentLevel(string path, string ip, int port)
+        {
+            IPEndPoint sourceEndPoint = new IPEndPoint(IPAddress.Any, SOURCE_PORT);
+            OscMessage message = new OscMessage(sourceEndPoint, path);
+            IPAddress destinationIp = IPAddress.Parse(ip);
+            IPEndPoint destinationEndPoint = new IPEndPoint(destinationIp, port);
+            byte[] msgBytes = message.ToByteArray();
+            udpConnection.Send(msgBytes, msgBytes.Length, destinationEndPoint);
+        }
+
+        public static void SetLevel(string path, float level, string ip, int port = DESTINATION_PORT)
+        {
+            IPEndPoint sourceEndPoint = new IPEndPoint(IPAddress.Any, SOURCE_PORT);
+            OscMessage message = new OscMessage(sourceEndPoint, path);
+            message.Append(level);
+            IPAddress destinationIp = IPAddress.Parse(ip);
+            IPEndPoint destinationEndPoint = new IPEndPoint(destinationIp, port);
+            byte[] msgBytes = message.ToByteArray();
+            udpConnection.Send(msgBytes, msgBytes.Length, destinationEndPoint);
+        }
+
+        private static Dictionary<string, List<TaskCompletionSource<float>>> requests = new();
+
+        private static string requestDictionaryKey(string path, string ip, int port) => $"{ip}:{port}#{path}";
+
+        private static List<TaskCompletionSource<float>> getRequestList(string path, string ip, int port)
+        {
+            string _requestDictionaryKey = requestDictionaryKey(path, ip, port);
+            if (!requests.TryGetValue(_requestDictionaryKey, out List<TaskCompletionSource<float>> requestList))
+            {
+                requestList = new();
+                requests.Add(_requestDictionaryKey, requestList);
+            }
+            return requestList;
+        }
+
+        private static TaskCompletionSource<float> getNextRequest(string path, string ip, int port)
+        {
+            if (!requests.TryGetValue(requestDictionaryKey(path, ip, port), out List<TaskCompletionSource<float>> requestList))
+                return null;
+            TaskCompletionSource<float> nextRequest = requestList.FirstOrDefault();
+            if (nextRequest != null)
+                requestList.RemoveAt(0);
+            return nextRequest;
+        }
+
+        private static TaskCompletionSource<float> addRequest(string path, string ip, int port)
+        {
+            List<TaskCompletionSource<float>> requestList = getRequestList(path, ip, port);
+            TaskCompletionSource<float> request = new();
+            requestList.Add(request);
+            return request;
+        }
+
+        private static UdpClient udpConnection;
+
+        public static void Init() => Task.Run(_init);
+
+        private static async Task _init()
+        {
+            if (udpConnection != null)
+                return;
+            udpConnection = new UdpClient(SOURCE_PORT);
+            while (true)
+            {
+                UdpReceiveResult result = await udpConnection.ReceiveAsync();
+                OscPacket packet = OscPacket.FromByteArray(result.RemoteEndPoint, result.Buffer);
+                if (!packet.IsBundle && (packet is OscMessage message))
+                    oscMessageReceived(message);
+            }
+        }
+
+        private static void oscMessageReceived(OscMessage message)
+        {
+            try
+            {
+                string sourceIP = message.SourceEndPoint.Address.ToString();
+                int sourcePort = message.SourceEndPoint.Port;
+                getNextRequest(message.Address, sourceIP, sourcePort)?.SetResult(message.At<float>(0));
+            }
+            catch { }
+        }
+
+
+    }
+}
