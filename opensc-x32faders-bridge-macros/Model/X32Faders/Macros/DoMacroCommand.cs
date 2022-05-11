d41b8bc674a0e8a8584f9de9ff54dd94ef202bbb
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2022-05-11 21:32:55 +0200

    Add macro for X32 faders

---
+++ opensc-x32faders-bridge-macros/Model/X32Faders/Macros/DoMacroCommand.cs
@@ -0,0 +1,26 @@
+﻿using OpenSC.Model.Macros;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenSC.Model.X32Faders.Macros
+{
+
+    [MacroCommand("X32Faders.Do", "X32 fade", "Do a fade event for an X32 fader.")]
+    public class DoMacroCommand : MacroCommandBase
+    {
+
+        protected override void _run(object[] argumentObjects) => (argumentObjects[0] as X32Fader)?.Do();
+
+        [MacroCommandArgument(0, "Fader", "Fader settings to use")]
+        public class Arg0 : MacroCommandArgumentDatabaseItem<X32Fader>
+        {
+            public Arg0() : base(X32FaderDatabase.Instance)
+            { }
+        }
+
+    }
+
+}
