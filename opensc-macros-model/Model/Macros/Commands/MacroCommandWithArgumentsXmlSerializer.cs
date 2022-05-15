0c92cc2d9d093151a367debf225f850cbd6d207e
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2022-05-15 11:07:02 +0200

    Fix: remove not restored (null) macro commands and triggers from macro's collection after restoration

--- opensc-macros-model/Model/Macros/Commands/MacroCommandWithArgumentsXmlSerializer.cs
+++ opensc-macros-model/Model/Macros/Commands/MacroCommandWithArgumentsXmlSerializer.cs
@@ -21,9 +21,7 @@ namespace OpenSC.Model.Macros
         {
             if (serializedItem.LocalName != TAG_NAME)
                 return null;
-
             string commandCode = serializedItem.Attributes[ATTRIBUTE_CODE]?.Value;
-
             List<string> argumentKeys = new List<string>();
             foreach (XmlNode childNode in serializedItem.ChildNodes)
             {
@@ -33,34 +31,24 @@ namespace OpenSC.Model.Macros
                     argumentKeys.Add(argumentKey);
                 }
             }
-
             IMacroCommand command = MacroCommandRegister.Instance.GetCommand(commandCode);
-            if (command == null)
-                return null;
-
-            return command.GetWithArgumentsByKeys(argumentKeys.ToArray());
-
+            return command?.GetWithArgumentsByKeys(argumentKeys.ToArray());
         }
 
         public XElement SerializeItem(object item, object parentItem, object[] indicesOrKeys)
         {
-
             MacroCommandWithArguments commandWA = item as MacroCommandWithArguments;
             if (commandWA == null)
                 return null;
-
             XElement xmlElement = new XElement(TAG_NAME);
             xmlElement.SetAttributeValue(ATTRIBUTE_CODE, commandWA.CommandCode);
-
             foreach (string argumentStr in commandWA.ArgumentKeys)
             {
                 XElement argElement = new XElement(TAG_NAME_ARGUMENT);
                 argElement.SetAttributeValue(ATTRIBUTE_ARGUMENT_KEY, argumentStr);
                 xmlElement.Add(argElement);
             }
-
             return xmlElement;
-
         }
 
     }
