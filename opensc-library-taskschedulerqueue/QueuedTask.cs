b0206e243727fdb10d55b56ded58a6a333ed6eed
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2022-05-15 12:45:24 +0200

    Add task scheduler queue library

---
+++ opensc-library-taskschedulerqueue/QueuedTask.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+
+namespace OpenSC.Library.TaskSchedulerQueue
+{
+
+    public abstract class QueuedTask<TResult>
+    {
+        protected internal virtual bool IsValid => true;
+        private TaskCompletionSource<TResult> taskCompletionSource = new();
+        public async Task Wait() => await taskCompletionSource.Task;
+        public void Cancel() => taskCompletionSource.SetCanceled();
+        public void Ready(TResult result)
+        {
+            taskCompletionSource.SetResult(result);
+            _ready(result);
+        }
+        protected virtual void _ready(TResult result) { }
+    }
+
+    public abstract class QueuedTask : QueuedTask<NullResult>
+    {
+        public void Ready() => base.Ready(null);
+    }
+
+}
