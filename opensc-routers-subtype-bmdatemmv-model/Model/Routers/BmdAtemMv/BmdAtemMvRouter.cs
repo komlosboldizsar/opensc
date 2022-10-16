72a589027323b994c10e9bee2deb833e93122e4a
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2022-10-16 12:54:29 +0200

    Add BMD ATEM multiviewer as router (model)

---
+++ opensc-routers-subtype-bmdatemmv-model/Model/Routers/BmdAtemMv/BmdAtemMvRouter.cs
@@ -0,0 +1,150 @@
+﻿using BMD.Switcher;
+using OpenSC.Logger;
+using OpenSC.Model.General;
+using OpenSC.Model.Mixers;
+using OpenSC.Model.Mixers.BlackMagicDesign;
+using OpenSC.Model.Persistence;
+using OpenSC.Model.Settings;
+using OpenSC.Model.SourceGenerators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OpenSC.Model.Routers.BmdAtemMv
+{
+
+    [TypeLabel("BMD ATEM Multiview")]
+    [TypeCode("bmdatemmv")]
+    public partial class BmdAtemMvRouter : Router
+    {
+
+        private new const string LOG_TAG = "Router/BmdAtemMv";
+
+        public override void RestoredOwnFields()
+        {
+            base.RestoredOwnFields();
+            if (mixer != null)
+                getMultiviews();
+        }
+
+        #region Property: Mixer
+        [PersistAs("mixer")]
+        [AutoProperty]
+        [AutoProperty.BeforeChange(nameof(_mixer_beforeChange))]
+        [AutoProperty.AfterChange(nameof(_mixer_afterChange))]
+        private Mixer mixer;
+
+        private void _mixer_beforeChange(Mixer oldValue, Mixer newValue, BeforeChangePropertyArgs args)
+        {
+            if (oldValue != null)
+                oldValue.StateChanged -= Mixer_StateChanged;
+        }
+
+        private void _mixer_afterChange(Mixer oldValue, Mixer newValue)
+        {
+            if (newValue != null)
+            {
+                newValue.StateChanged += Mixer_StateChanged;
+                getMultiviews();
+            }
+        }
+
+        private void Mixer_StateChanged(Mixer item, MixerState oldValue, MixerState newValue)
+            => getMultiviews();
+
+        private void Multiview_WindowSourceChanged(BMDSwitcherAPI.IBMDSwitcherMultiView apiMultiview, Multiview multiview, uint windowIndex, long sourceId)
+            => handleWindowSourceChange(multiview.Index, (int)windowIndex, sourceId);
+        #endregion
+
+        #region Multiviews of mixer
+        private readonly List<Multiview> multiviews = new();
+
+        private void getMultiviews()
+        {
+            BmdMixer bmdMixer = (BmdMixer)mixer;
+            foreach (Multiview multiview in multiviews)
+                multiview.WindowSourceChanged -= Multiview_WindowSourceChanged;
+            multiviews.Clear();
+            multiviews.AddRange(bmdMixer.ApiSwitcher.GetAllMultiviews());
+            foreach (Multiview multiview in multiviews)
+                multiview.WindowSourceChanged += Multiview_WindowSourceChanged;
+            queryAllStates();
+        }
+        #endregion
+
+        #region Input and output instantiation
+        public override RouterInput CreateInput(string name, int index)
+            => new(name, this, index);
+
+        public override RouterOutput CreateOutput(string name, int index)
+            => new BmdAtemMvRouterOutput(name, this, index);
+
+        private static readonly Dictionary<Type, string> OUTPUT_TYPES = new()
+        {
+            { typeof(BmdAtemMvRouterOutput), "bmdatemmv" }
+        };
+
+        protected override Dictionary<Type, string> OutputTypesDictionaryGetter()
+            => OUTPUT_TYPES;
+        #endregion
+
+        #region Setting/getting crosspoints
+        protected override void requestCrosspointUpdateImpl(RouterOutput output, RouterInput input)
+        {
+            int multiviewIndex = output.Index / 100;
+            if ((multiviewIndex < 0) || (multiviewIndex >= multiviews.Count))
+                throw new ArgumentException($"Invalid output: multiviewer with index #{multiviewIndex} unknown.", nameof(output));
+            Multiview multiview = multiviews[multiviewIndex];
+            int windowIndex = output.Index % 100;
+            if ((windowIndex < 0) || (windowIndex >= multiview.WindowCount))
+                throw new ArgumentException($"Invalid output: window with index #{windowIndex} unknown at multiviewer #{multiviewIndex}.", nameof(output));
+            multiview.RequestSetWindowSourceId((uint)windowIndex, input.Index);
+        }
+
+        protected override void requestCrosspointUpdatesImpl(IEnumerable<RouterCrosspoint> crosspoints)
+        {
+            foreach (RouterCrosspoint crosspoint in crosspoints)
+                requestCrosspointUpdateImpl(crosspoint.Output, crosspoint.Input);
+        }
+
+        protected override void queryAllStates()
+        {
+            RouterInput unknownInput = getUnknownInput();
+            int multiviewIndex = 0;
+            foreach (Multiview multiview in multiviews)
+            {
+                long[] windowSourceIds = multiview.GetAllWindowSourceIds();
+                for (int windowIndex = 0; windowIndex < windowSourceIds.Length; windowIndex++)
+                    handleWindowSourceChange(multiviewIndex, windowIndex, windowSourceIds[windowIndex], true, unknownInput);
+                multiviewIndex++;
+            }
+        }
+
+        private void handleWindowSourceChange(int multiviewIndex, int windowIndex, long sourceId, bool unknownInputGiven = false, RouterInput unknownInput = null)
+        {
+            if (!unknownInputGiven)
+                unknownInput = getUnknownInput();
+            int outputIndex = multiviewIndex * 100 + windowIndex;
+            RouterOutput output = Outputs.FirstOrDefault(o => o.Index == outputIndex);
+            if (output != null)
+            {
+                RouterInput input = Inputs.FirstOrDefault(i => i.Index == sourceId);
+                output.AssignSource(input ?? unknownInput);
+            }
+        }
+
+        private RouterInput getUnknownInput()
+            => Inputs.FirstOrDefault(i => i.Index == UNKNOWN_INPUT_INDEX);
+
+        public const int UNKNOWN_INPUT_INDEX = 999;
+
+        protected override void requestLockOperationImpl(RouterOutput output, RouterOutputLockType lockType, RouterOutputLockOperationType lockOperationType)
+            => throw new NotImplementedException();
+        #endregion
+
+    }
+
+}
---
+++ opensc-routers-subtype-bmdatemmv-model/Model/Routers/BmdAtemMv/BmdAtemMvRouterOutputXmlSerializer.cs
@@ -0,0 +1,31 @@
+﻿using OpenSC.Model.Persistence;
+using System;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace OpenSC.Model.Routers.BmdAtemMv
+{
+
+    public class BmdAtemMvRouterOutputXmlSerializer : RouterOutputXmlSerializer
+    {
+
+        public override Type Type => typeof(BmdAtemMvRouterOutput);
+
+        public override object DeserializeItem(XmlNode serializedItem, object parentItem, object[] keysOrIndices)
+        {
+            BmdAtemMvRouterOutput restoredOutput = base.DeserializeItem(serializedItem, parentItem, keysOrIndices) as BmdAtemMvRouterOutput;
+            return restoredOutput;
+        }
+
+        public override XElement SerializeItem(object item, object parentItem, object[] keysOrIndices)
+        {
+            BmdAtemMvRouterOutput output = item as BmdAtemMvRouterOutput;
+            if (output == null)
+                return null;
+            XElement serializedOutput = base.SerializeItem(item, parentItem, keysOrIndices);
+            return serializedOutput;
+        }
+
+    }
+
+}
